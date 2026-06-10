ed5dddb7ba52ae2a1e062b7cb4253b694b4b6b96
PAULLLE14 <paulocker14@gmail.com>
2026-06-10 14:55:06 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@ import board
 from adafruit_pca9685 import PCA9685
 import time
 import logging
-import RPi.GPIO as GPIO
+from gpiozero import LineSensor
 
 log = logging.getLogger(__name__)
-
 i2c = board.I2C()
-
 pca = PCA9685(i2c)
 
 current_speed_front_left = 0
@@ -15,11 +13,11 @@ current_speed_front_right = 0
 current_speed_rear_left = 0
 current_speed_front_right = 0
 
-SENSOR_LEFT   = 23
-SENSOR_MIDDLE = 15
-SENSOR_RIGHT  = 14
+sensor_left   = LineSensor(23)
+sensor_middle = LineSensor(15)
+sensor_right  = LineSensor(14)
 
-def init ():
+def init():
     log.info("initialize the PWM module")
     pca.frequency = 50
     pca.channels[0].duty_cycle = 0
@@ -30,12 +28,6 @@ def init ():
     pca.channels[5].duty_cycle = 0
     pca.channels[6].duty_cycle = 0
     pca.channels[7].duty_cycle = 0
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SENSOR_LEFT,   GPIO.IN)
-    GPIO.setup(SENSOR_MIDDLE, GPIO.IN)
-    GPIO.setup(SENSOR_RIGHT,  GPIO.IN)
-
     pass
 
 def stop_all():
@@ -57,10 +49,8 @@ def front_left(speed=0):
     if 0 > abs(speed) > 100:
         log.error(f"speed {speed} outside of range 0-100")
         return
-
     motor_speed = int((abs(speed) * 0xFFFF) / 100)
     current_speed_front_left = speed
-
     if speed >= 0:
         pca.channels[0].duty_cycle = 0
         pca.channels[1].duty_cycle = motor_speed
@@ -68,15 +58,12 @@ def front_left(speed=0):
         pca.channels[0].duty_cycle = motor_speed
         pca.channels[1].duty_cycle = 0
 
-
 def rear_left(speed=0):
     if 0 > abs(speed) > 100:
         log.error(f"speed {speed} outside of range 0-100")
         return
-
     motor_speed = int((abs(speed) * 0xFFFF) / 100)
     current_speed_rear_left = speed
-
     if speed >= 0:
         pca.channels[3].duty_cycle = 0
         pca.channels[2].duty_cycle = motor_speed
@@ -84,15 +71,12 @@ def rear_left(speed=0):
         pca.channels[3].duty_cycle = motor_speed
         pca.channels[2].duty_cycle = 0
 
-
 def rear_right(speed=0):
     if 0 > abs(speed) > 100:
         log.error(f"speed {speed} outside of range 0-100")
         return
-
     motor_speed = int((abs(speed) * 0xFFFF) / 100)
     current_speed_rear_right = speed
-
     if speed >= 0:
         pca.channels[4].duty_cycle = 0
         pca.channels[5].duty_cycle = motor_speed
@@ -100,15 +84,12 @@ def rear_right(speed=0):
         pca.channels[4].duty_cycle = motor_speed
         pca.channels[5].duty_cycle = 0
 
-
 def front_right(speed=0):
     if 0 > abs(speed) > 100:
         log.error(f"speed {speed} outside of range 0-100")
         return
-
     motor_speed = int((abs(speed) * 0xFFFF) / 100)
     current_speed_front_right = speed
-
     if speed >= 0:
         pca.channels[7].duty_cycle = 0
         pca.channels[6].duty_cycle = motor_speed
@@ -117,29 +98,40 @@ def front_right(speed=0):
         pca.channels[6].duty_cycle = 0
 
 def read_sensors():
-    left   = GPIO.input(SENSOR_LEFT)
-    middle = GPIO.input(SENSOR_MIDDLE)
-    right  = GPIO.input(SENSOR_RIGHT)
+    left   = sensor_left.value
+    middle = sensor_middle.value
+    right  = sensor_right.value
     return left, middle, right
+
+def drive_forward():
+    front_left(20)
+    rear_left(20)
+    front_right(20)
+    rear_right(20)
+
+def turn_left():
+    front_left(20)
+    rear_left(20)
+    front_right(-25)
+    rear_right(-25)
+
+def turn_right():
+    front_left(-25)
+    rear_left(-25)
+    front_right(20)
+    rear_right(20)
 
 def drive():
     left, middle, right = read_sensors()
-
     if middle == 1:
-        front_left(50)
-        rear_left(50)
-        front_right(50)
-        rear_right(50)
-    elif middle ==0:
-        front_left(0)
-        rear_left(0)
-        front_right(0)
-        rear_right(0)
+        drive_forward()
+    elif left == 1:
+        turn_left()
+    elif right == 1:
+        turn_right()
 
 init()
-
 start = time.time()
-while time.time() - start < 4:
+while time.time() - start < 60:
     drive()
-
 stop_all()
